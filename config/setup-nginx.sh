20cab5017be2a62271fa71bdc016c0ad197ed64c
PuZZleDucK <puzzleduck@gmail.com>
2017-06-04 02:24:53 +1000

    tweak setup

--- config/setup-nginx.sh
+++ config/setup-nginx.sh
@@ -2,13 +2,13 @@
 mkdir /home/user/backups
 
 echo "BACKING UP CONFIG"
-cp -f /etc/nginx/sites-avaliable/puzzleduck.org /home/user/backups/
-cp -f /etc/nginx/sites-avaliable/test.org /home/user/backups/
+cp -f /etc/nginx/sites-available/puzzleduck.org /home/user/backups/
+cp -f /etc/nginx/sites-available/test.org /home/user/backups/
 cp -f /etc/nginx/nginx.conf /home/user/backups/
 
 echo "INSTALLING NEW CONFIG"
-sudo cp -f config/nginx/sites-avaliable/puzzleduck.org /etc/nginx/sites-avaliable/puzzleduck.org
-sudo cp -f config/nginx/sites-avaliable/test.org /etc/nginx/sites-avaliable/test.org
+sudo cp -f config/nginx/sites-available/puzzleduck.org /etc/nginx/sites-available/puzzleduck.org
+sudo cp -f config/nginx/sites-available/test.org /etc/nginx/sites-available/test.org
 sudo cp -f config/nginx/nginx.conf /etc/nginx/nginx.conf
 
 echo "RESTARTING NGINX"
