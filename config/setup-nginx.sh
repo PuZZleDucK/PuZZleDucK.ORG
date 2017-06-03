a2619b80e8bf16ee908e09f4c18cd4eb08556413
PuZZleDucK <puzzleduck@gmail.com>
2017-06-04 08:07:53 +1000

    remove invalid include and add config test

--- config/setup-nginx.sh
+++ config/setup-nginx.sh
@@ -2,15 +2,18 @@
 mkdir /home/user/backups
 
 STAMP=`date --rfc-3339=ns | tr "+ ." "_"`
-echo "BACKING UP CONFIG"
-cp -f /etc/nginx/sites-available/puzzleduck.org /home/user/backups/"$STAMP"_puzzleduck.org
-cp -f /etc/nginx/sites-available/test.org /home/user/backups/"$STAMP"_test.org
-cp -f /etc/nginx/nginx.conf /home/user/backups/"$STAMP"_nginx.conf
+echo ":: BACKING UP CONFIG"
+cp -vf /etc/nginx/sites-available/puzzleduck.org /home/user/backups/"$STAMP"_puzzleduck.org
+cp -vf /etc/nginx/sites-available/test.org /home/user/backups/"$STAMP"_test.org
+cp -vf /etc/nginx/nginx.conf /home/user/backups/"$STAMP"_nginx.conf
 
-echo "INSTALLING NEW CONFIG"
-sudo cp -f config/nginx/sites-available/puzzleduck.org /etc/nginx/sites-available/puzzleduck.org
-sudo cp -f config/nginx/sites-available/test.org /etc/nginx/sites-available/test.org
-sudo cp -f config/nginx/nginx.conf /etc/nginx/nginx.conf
+echo ":: INSTALLING NEW CONFIG"
+sudo cp -vf config/nginx/sites-available/puzzleduck.org /etc/nginx/sites-available/puzzleduck.org
+sudo cp -vf config/nginx/sites-available/test.org /etc/nginx/sites-available/test.org
+sudo cp -vf config/nginx/nginx.conf /etc/nginx/nginx.conf
 
-echo "RESTARTING NGINX"
+echo ":: TESTING NGINX"
+sudo nginx -t -c /var/www/puzzleduck.org/config/nginx/nginx.conf
+
+echo ":: RESTARTING NGINX"
 sudo nginx -s reload
