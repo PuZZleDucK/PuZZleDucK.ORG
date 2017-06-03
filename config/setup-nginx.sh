5eb8ee92018c004ab474373b61a3e36171282df1
PuZZleDucK <puzzleduck@gmail.com>
2017-06-04 02:13:16 +1000

    clean up cruft

--- config/setup-nginx.sh
+++ config/setup-nginx.sh
@@ -8,7 +8,7 @@ cp -f /etc/nginx/sites-available/test.org /home/user/backups/
 cp -f /etc/nginx/nginx.conf /home/user/backups/
 
 echo "INSTALLING NEW CONFIG"
-sudo cp -f nginx/sites-available/origional-default /etc/nginx/sites-available/origional-default
+sudo rm -f /etc/nginx/sites-available/origional-default
 sudo cp -f nginx/sites-available/puzzleduck.org /etc/nginx/sites-available/puzzleduck.org
 sudo cp -f nginx/sites-available/test.org /etc/nginx/sites-available/test.org
 sudo cp -f nginx/nginx.conf /etc/nginx/nginx.conf
