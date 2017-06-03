4d557df1c51b8ab93378cbda76193a8394ec8663
PuZZleDucK <puzzleduck@gmail.com>
2017-06-04 03:12:29 +1000

    setup server on 8080 and update config script

--- config/setup-nginx.sh
+++ config/setup-nginx.sh
@@ -1,10 +1,11 @@
-
+#!/bin/bash
 mkdir /home/user/backups
 
+STAMP=`date --rfc-3339=ns | tr "+ ." "_"`
 echo "BACKING UP CONFIG"
-cp -f /etc/nginx/sites-available/puzzleduck.org /home/user/backups/
-cp -f /etc/nginx/sites-available/test.org /home/user/backups/
-cp -f /etc/nginx/nginx.conf /home/user/backups/
+cp -f /etc/nginx/sites-available/puzzleduck.org /home/user/backups/"$STAMP"_puzzleduck.org
+cp -f /etc/nginx/sites-available/test.org /home/user/backups/"$STAMP"_test.org
+cp -f /etc/nginx/nginx.conf /home/user/backups/"$STAMP"_nginx.conf
 
 echo "INSTALLING NEW CONFIG"
 sudo cp -f config/nginx/sites-available/puzzleduck.org /etc/nginx/sites-available/puzzleduck.org
