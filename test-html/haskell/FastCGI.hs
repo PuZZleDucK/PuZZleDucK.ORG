28d9b0aed5bd00018963f0eb0f889db866886a4d
PuZZleDucK <puzzleduck@gmail.com>
2017-06-05 23:44:31 +1000

    add Haskell fastCGI

---
+++ test-html/haskell/FastCGI.hs
@@ -0,0 +1,18 @@
+module Main ( main ) where
+
+import Control.Concurrent
+import Network.FastCGI
+
+action :: CGI CGIResult
+action = do
+    setHeader "Content-type" "text/plain"
+    tid <- liftIO myThreadId
+    output $ unlines
+        [ "I am a FastCGI process!"
+        , "Hear me roar!"
+        , ""
+        , show tid
+        ]
+
+main :: IO ()
+main = runFastCGIConcurrent' forkIO 10 action
