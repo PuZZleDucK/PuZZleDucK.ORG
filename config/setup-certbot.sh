8c303004d6a7c3116bf6d3e341f957274fe11f5d
PuZZleDucK <puzzleduck@gmail.com>
2017-08-13 06:43:35 +1000

    fix domain name and http refs

--- config/setup-certbot.sh
+++ config/setup-certbot.sh
@@ -16,12 +16,12 @@ sudo ufw allow 'Nginx Full'
 echo
 echo ":: INTERACTIVE: Run certbot"
 echo "Now run:"
-echo "    sudo certbot --nginx -d puzzleduck.com -d www.puzzleduck.com"
+echo "    sudo certbot --nginx -d puzzleduck.org"
 echo "Then backup config:"
-echo "    sudo tar zcvf /home/user/letsencrypt_backup_$(date +'%Y-%m-%d_%H%M').tar.gz /etc/letsencrypt"
-echo "    tar tvf /home/user/letsencrypt_backup_<date>.tar.gz"
+echo "    sudo tar zcvf /home/user/backups/letsencrypt_backup_$(date +'%Y-%m-%d_%H%M').tar.gz /etc/letsencrypt"
+echo "    tar tvf /home/user/backups/letsencrypt_backup_<date>.tar.gz"
 echo "To restore:"
-echo "    tar zxvf /home/user/letsencrypt_backup_<date>.tar.gz -C /"
+echo "    tar zxvf /home/user/backups/letsencrypt_backup_<date>.tar.gz -C /"
 
 echo
 echo ":: UPDATE Paramaters"
